2318af0f6392427d3cb79bf6577324be3b10540d
AkshayaSakthivel <akshaya.sakthivel.18@gmail.com>
2021-06-22 18:48:26 +0530

    meta table query changes

--- xAudit/xAudit.CDC/ReplicatorUsingCDC.cs
+++ xAudit/xAudit.CDC/ReplicatorUsingCDC.cs
@@ -79,6 +79,7 @@ namespace xAudit.CDC
         {
             var installer = new InstallerWithCDC(this.CurrentVersion,this._sqlServerDriver);
             await installer.InstallAsync(this._options.InstanceName);
+            await installer.UpdateAsync(this._options.InstanceName);
         }
         private Task ExecuteVersionUpdateScriptsAsync()
         {

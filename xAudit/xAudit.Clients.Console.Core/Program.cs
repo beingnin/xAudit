723cd751a1c7c0a79d17c9b07dc44e13cca15a65
AkshayaSakthivel <akshaya.sakthivel.18@gmail.com>
2021-09-05 18:46:32 +0530

    fluent api configuration changed

--- xAudit/xAudit.Clients.Console.Core/Program.cs
+++ xAudit/xAudit.Clients.Console.Core/Program.cs
@@ -13,7 +13,7 @@ namespace xAudit.Clients.Console.Core
                @"Data Source=10.10.100.68\SQL2016;Initial Catalog=SharjahPolice_Live_Beta_New;User ID=spsauser;Password=$P$@789#",
                @"Data Source=10.10.100.68\SQL2016;Initial Catalog=SharjahPolice_Live_Beta_New;User ID=spsauser;Password=$P$@789#")
                             .UseCDC()
-                            .TrackSchemaChanges()
+                            .DoNotTrackSchemaChanges()
                             .GetReplicator();
             try
             {

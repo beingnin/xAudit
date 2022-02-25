cb6b70cf114d06d891f59c55e1de45d482cff2e3
AkshayaSakthivel <akshaya.sakthivel.18@gmail.com>
2022-02-25 17:06:48 +0530

    added method definition for persist log

--- xAudit/xAudit.CDC/Helpers/LogRunner.cs
+++ xAudit/xAudit.CDC/Helpers/LogRunner.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.Common;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,9 +33,52 @@ namespace xAudit.CDC.Helpers
             _run.Logs.Add(log);
         }
 
-        public void Persist()
+        public async Task<int> Persist(Run run)
         {
-            //save run in db 
+            DataTable runTables = new DataTable();
+            runTables.Columns.Add("Schema", typeof(string));
+            runTables.Columns.Add("Name", typeof(string));
+            runTables.Columns.Add("FullName", typeof(string));
+            runTables.Columns.Add("Action", typeof(RunAction));
+            if (run.RunTables != null && run.RunTables.Count != 0)
+            {
+                foreach (var table in run.RunTables)
+                {
+                    runTables.Rows.Add(
+                        table.Schema,
+                        table.Name,
+                        table.FullName,
+                        table.Action);
+                }
+            }
+
+            DataTable logs = new DataTable();
+            logs.Columns.Add("Message", typeof(string));
+            logs.Columns.Add("Type", typeof(MessageType));
+            logs.Columns.Add("Exception", typeof(string));
+            logs.Columns.Add("StackTrace", typeof(string));
+            if (run.Logs != null && run.Logs.Count != 0)
+            {
+                foreach(var log in run.Logs)
+                {
+                    logs.Rows.Add(
+                        log.Message,
+                        log.Type,
+                        log.Exception,
+                        log.StackTrace);
+                }
+            }
+
+            IDbDataParameter[] dbDataParameter =
+            {
+                new SqlParameter("@TrackSchemaChanges",run.TrackSchemaChanges),
+                new SqlParameter("@ForceMerge",run.ForceMerge),
+                new SqlParameter("@InstanceName",run.InstanceName),
+                new SqlParameter("@MachineName",run.MachineName),
+                new SqlParameter("@RunTables",runTables),
+                new SqlParameter("@Logs",logs)
+            };
+            return await _sqlServerDriver.ExecuteNonQueryAsync(run.InstanceName + ".INSERT_NEW_Run", dbDataParameter);
         }
     }
 }
